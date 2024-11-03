9f83580e00e5606fb52b2998b6608f543a95430f
MaksimGulyaev <gulyaev_mo@vk.com>
2024-11-03 23:33:44 +0300

    Create Lw3_1.py

---
+++ Lw3/Lw3_1.py
@@ -0,0 +1,15 @@
+
+def search_items(list_arg:list, item_arg):
+    l = len(list_arg)
+    for item in range (0,l-1):
+        if list_arg[item] == item_arg:
+            return item
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = search_items(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
